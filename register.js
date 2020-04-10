e9acd2e900d098aadf454209fd7b828b86619cf6
AxlM <axlmonreal@gmail.com>
2020-04-09 18:26:12 -0700

    Add bootstrap to register.html

--- register.js
+++ register.js
@@ -1,27 +1,5 @@
-/*
-var check = function() {
-
-  if (document.getElementById('password').value == document.getElementById('con_password').value) {
-		    document.getElementById('msg').style.color = 'green';
-		    document.getElementById('msg').innerHTML = 'matching';
-  } 
-  else {
-		    document.getElementById('msg').style.color = 'red';
-		    document.getElementById('msg').innerHTML = 'not matching';
-  }
-}
-*/
-
 // A $( document ).ready() block.
 $( document ).ready(function() {
-		$( "#dialog" ).dialog({
-		    autoOpen: false,
-		      buttons: {
-		        Ok: function() {
-		          $( this ).dialog( "close" );
-		        }
-		      }
-		  });
 		$( "#button" ).click(function() {
 
 					$.ajax({
@@ -33,9 +11,10 @@ $( document ).ready(function() {
 						    password: $('#password').val(),
 						    con_password: $('#con_password').val()
 						  },
-						  success: function( result ) {
-				    
-							$('#dialog').html(result).dialog('open');
+						  success: function( result ) {					
+
+							$('#msg').html(result); 
+							$('#dialog').modal('show'); 
 
 
 							 
